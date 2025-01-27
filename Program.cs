e42816d71e99b56820bc3bb5186ad659ef88042d
jackrod13 <120751056+jackrod13@users.noreply.github.com>
2025-01-27 08:18:15 -0700

    Commented Main Program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Welcome User to the Game 
+
+// Create a board game array to store the players choices
+
+// Ask each player during their turn for their choice and update the game board 
+
+// Print the board by calling the method in the supporting class
+
+// Check for the winner by calling the method in the supporting class
+
+// Notify the players when a win has occured and which player has won
