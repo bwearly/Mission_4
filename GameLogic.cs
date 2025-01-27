47bff4de45e1517da882c0f2877a611c060461ba
bwearly <153124661+bwearly@users.noreply.github.com>
2025-01-27 08:47:15 -0700

    added some comments

--- GameLogic.cs
+++ GameLogic.cs
@@ -15,19 +15,23 @@ namespace GameLogic
     {
         public char CheckWinner(char[,] board)
         {
+            // For loop to make sure that there is three in a row
             for (int i = 0; i < 3; i++)
             {
+                // 
                 if (board[i, 0] == ' ' && board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2])
                     return board[i, 0];
                 if (board[0, i] != ' ' && board[0, i] == board[1, i] && board[1, i] == board[2, i])
                     return board[0, i];
             }
 
+            // 
             if (board[0, 0] != ' ' && board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
                 return board[0, 0];
             if (board[0, 2] != ' ' && board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
                 return board[0, 2];
 
+            // Returns the winner
             return ' ';
         }
 
