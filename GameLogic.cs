e84eeb031e79fb669c7a5701ded6c09bcfefb95f
bwearly <153124661+bwearly@users.noreply.github.com>
2025-01-27 08:17:58 -0700

    created a class and added comments

---
+++ GameLogic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLogic
+{
+    // recieve the board array from the driver class
+
+    // contain a method that prints the board based on the information passed to it
+
+    // Contain the method that recieves the game board array as input and returns if there is a winner and who it was
+    internal class GameLogic
+    {
+
+
+    }
+}
